2e7645db4124894f10d8a6aea52c24c285c53449
jamesdosstech <jamesdosstech@gmail.com>
2023-04-03 19:25:03 -0500

    install tailwind css

---
+++ imdb-clone/src/app/head.js
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Imdb Clone</title>
+      <meta
+        content="width=device-width, initial-scale=1"
+        name="viewport"
+      ></meta>
+      <meta name="description" content="imdb clone" />
+      <link rel="icon" href="/favicon.png" />
+    </>
+  );
+}
